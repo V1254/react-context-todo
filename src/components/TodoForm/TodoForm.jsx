8c818e9844797d42666442b298136d643c8e6b6b
Mo <33751344+V1254@users.noreply.github.com>
2020-08-07 15:46:24 +0100

    :heavy_plus_sign: add form component

---
+++ src/components/TodoForm/TodoForm.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const TodoForm = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`submitting ${title} and ${body}`);
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        id="title"
+        placeholder="Todo Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        id="body"
+        placeholder="Todo Body"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default TodoForm;
