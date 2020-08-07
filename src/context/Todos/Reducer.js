0b671a1c9b12a4b0c3270eb7bd15c666080f66b2
Mo <33751344+V1254@users.noreply.github.com>
2020-08-07 17:17:34 +0100

    :construction_worker: replace with array concat

--- src/context/Todos/Reducer.js
+++ src/context/Todos/Reducer.js
@@ -3,13 +3,10 @@ import { SAVE_TODO, DELETE_TODO } from "./actionTypes";
 const reducer = (state, action) => {
   switch (action.type) {
     case SAVE_TODO:
-      return [
-        ...state,
-        {
-          id: state.length,
-          ...action.payload,
-        },
-      ];
+      return state.concat({
+        id: state.length,
+        ...action.payload,
+      });
     case DELETE_TODO:
       return state.filter((todo) => todo.id !== action.payload.id);
 
