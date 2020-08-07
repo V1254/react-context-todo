cf32de73749f763010a0aef41ea5b4f172bc3222
Mo <33751344+V1254@users.noreply.github.com>
2020-08-07 16:36:55 +0100

    add reducer and action types

---
+++ src/context/Todos/Reducer.js
@@ -0,0 +1,21 @@
+import { SAVE_TODO, DELETE_TODO } from "./actionTypes";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SAVE_TODO:
+      return [
+        ...state,
+        {
+          id: state.length,
+          ...action.payload,
+        },
+      ];
+    case DELETE_TODO:
+      return state.filter((todo) => todo.id !== action.payload.id);
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
---
+++ src/context/Todos/actionTypes.js
@@ -0,0 +1,2 @@
+export const SAVE_TODO = "SAVE_TODO";
+export const DELETE_TODO = "DEL_TODO";
